64fdb2df2d7442f85c7202347d4bb8039ccf791e
Your Name <you@example.com>
2024-11-16 09:55:34 +0530

    first commit

---
+++ rutu.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+
+
+ printf ("enter the value of x");
+ scanf("%d",&x);
+ 
+ printf("enter the value of y");
+ scanf("%d",&y);
+ 
+ printf("x value %d \n",y);
+ printf("y value %d \n",x);
+return 0;
+}
